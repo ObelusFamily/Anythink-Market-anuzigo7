9c5651409781c4fd855f880ee1032796841756e6
Jordan Roberts <jrobertswebdev@gmail.com>
2022-11-30 05:17:41 +0000

    search field added to main view

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,15 +1,19 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import { Search } from "./Search";
 
 const Banner = () => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
-        <div>
-          <span>A place to </span>
-          <span id="get-part">get</span>
-          <span> the cool stuff.</span>
+        <div className="container">
+        <div className="row">
+        <h3 className="col text-right">A place to get</h3>        
+            <Search />
+        <h3 className="col text-left"> the cool stuff.</h3>
+          </div>
+          
         </div>
       </div>
     </div>
---
+++ frontend/src/components/Home/Search.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Search = () => {
+  return (<>
+    <input type="text" className="col form-control" placeholder='What is it that you truly desire?' />
+    </>
+  )
+}
